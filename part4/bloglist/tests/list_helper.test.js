dd5af956eccb79d1eda4838740209de118e2ab95
Jonas Beck <jona63m2@edu.campusvejle.dk>
2024-04-02 11:47:21 +0200

    4.5: Helper Functions and Unit Tests, step 3

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -2,63 +2,84 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
-test("Dummy returns one", () => {
-  const blogs = [];
+// Create test data
+const bigList = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  },
+];
 
-  const result = listHelper.dummy(blogs);
+const listWithTwoFavorites = [
+  {
+    title: "Building a second braind",
+    author: "Tiago Forte",
+    url: "localhost:3001",
+    likes: 5,
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 5,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  },
+];
+
+const listWithOneBlog = [
+  {
+    title: "Building a second braind",
+    author: "Tiago Forte",
+    url: "localhost:3001",
+    likes: 5,
+  },
+];
+
+const emptyList = [];
+
+test("Dummy returns one", () => {
+  const result = listHelper.dummy(emptyList);
   assert.strictEqual(result, 1);
 });
 
 describe("total likes", () => {
-  const bigList = [
-    {
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-    },
-    {
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-    },
-    {
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-    },
-    {
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-    },
-    {
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-    },
-    {
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-    },
-  ];
-  const listWithOneBlog = [
-    {
-      title: "Building a second braind",
-      author: "Tiago Forte",
-      url: "localhost:3001",
-      likes: 5,
-    },
-  ];
-
-  const emptyList = [];
-
   test("when list has only one blog equals the likes of that", () => {
     const result = listHelper.totalLikes(listWithOneBlog);
     assert.strictEqual(result, 5);
@@ -77,5 +98,32 @@ describe("total likes", () => {
   test("of a bigger list is calculated right", () => {
     const result = listHelper.totalLikes(bigList);
     assert.strictEqual(result, 36);
+  });
+});
+
+describe("favorite blog", () => {
+  test("should return null if list is empty", () => {
+    const result = listHelper.favoriteBlog(emptyList);
+    assert.strictEqual(result, null);
+  });
+
+  test("should return null when no list is provided", () => {
+    const result = listHelper.favoriteBlog();
+    assert.strictEqual(result, null);
+  });
+
+  test("when list has only one blog it should return that blog", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(result, listWithOneBlog[0]);
+  });
+
+  test("when list have multiple blogs should return the one with most likes", () => {
+    const result = listHelper.favoriteBlog(bigList);
+    assert.deepStrictEqual(result, bigList[2]);
+  });
+
+  test("when list have two blogs with most likes should return the first one", () => {
+    const result = listHelper.favoriteBlog(listWithTwoFavorites);
+    assert.deepStrictEqual(result, listWithTwoFavorites[0]);
   });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,5 @@
-const dummy = (blogs) => {
+// eslint-disable-next-line no-unused-vars
+const dummy = (_blogs) => {
   return 1;
 };
 
@@ -14,7 +15,21 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  // Check if blogs is empty or undefined
+  if (!blogs || blogs.length === 0) {
+    return null;
+  }
+
+  // Return blog object with most likes
+  return blogs.reduce((mostLikes, current) =>
+    // Ternary operator to check which blog to return
+    mostLikes.likes >= current.likes ? mostLikes : current,
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
