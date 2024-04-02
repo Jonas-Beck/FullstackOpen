9db9e1732f6e53e2947383df1833f9c8c83d2eca
Jonas Beck <jona63m2@edu.campusvejle.dk>
2024-04-02 09:54:28 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,74 @@ test("Dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  const bigList = [
+    {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+    },
+    {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    },
+    {
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+    },
+    {
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+    },
+    {
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+    },
+  ];
+  const listWithOneBlog = [
+    {
+      title: "Building a second braind",
+      author: "Tiago Forte",
+      url: "localhost:3001",
+      likes: 5,
+    },
+  ];
+
+  const emptyList = [];
+
+  test("when list has only one blog equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes(emptyList);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when no list is provided is zero", () => {
+    const result = listHelper.totalLikes(emptyList);
+    assert.strictEqual(result, 0);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(bigList);
+    assert.strictEqual(result, 36);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  // If there are no blogs or if blogs array is empty, return 0 likes
+  if (!blogs || blogs.length === 0) {
+    return 0;
+  }
+
+  // Use reduce to sum up the likes of all blogs
+  return blogs.reduce((accumulator, current) => {
+    return accumulator + current.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
